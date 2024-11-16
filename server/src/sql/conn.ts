5a34bcc32d6571721a2d132789a8af26007466be
Dwight Thorpe <dthorpe@day2dayja.com>
2024-11-16 09:32:30 -0500

    sighhhh

--- server/src/sql/conn.ts
+++ server/src/sql/conn.ts
@@ -2,47 +2,35 @@ import mysql, { Connection, ConnectionOptions } from "mysql2/promise";
 
 export const admin_pool = async () =>
   await getAdminConnection({
-    host: process.env.MYSQL_HOST || "localhost",
-    user: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASS || "",
-    database: process.env.MYSQL_DB,
-    port: process.env.MYSQL_PORT || "3306",
+    host: "localhost",
+    user: "root",
+    password: process.env.MYSQL_ROOT_PASSWORD || "",
+    database: process.env.MYSQL_DATABASE,
   });
 
 export const admin_db_pool = async () =>
   await getAdminConnection({
-    host: process.env.MYSQL_HOST || "localhost",
-    user: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASS || "",
-    database: process.env.MYSQL_DB,
-    port: process.env.MYSQL_PORT || "3306",
+    host: "localhost",
+    user: "root",
+    password: process.env.MYSQL_ROOT_PASSWORD || "",
+    database: process.env.MYSQL_DATABASE,
   });
 
 async function getAdminConnection({
   host,
-  port,
+
   database,
   user,
   password,
 }: {
   host: string;
-  port: string;
   database?: string;
   user: string;
   password: string;
 }): Promise<Connection> {
-  let portNum = 0;
-
-  try {
-    parseInt(port);
-  } catch (error: unknown) {
-    console.log("SQL PORT ERROR", { error });
-    portNum = 0;
-  }
-
   const connOps: ConnectionOptions = {
     host,
-    port: portNum,
+    port: 3959,
     user,
     password,
   };
